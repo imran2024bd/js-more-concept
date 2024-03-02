3c5bf62c29736fa92169efce87cd035fdfd543c5
imran2024bd <imranitbd34@gmail.com>
2024-03-02 09:34:18 +0600

    JavaScript Event Loop And Concurrency

---
+++ event-loop.js
@@ -0,0 +1,29 @@
+
+/* task -01 */
+function a(){
+    console.log('a');
+}
+
+function b(){
+    console.log('b');
+}
+// a();
+// b();
+
+
+/* task -02 */
+function a(){
+    console.log('a');
+    b();
+    console.log('aa');
+}
+
+function b(){
+    console.log('b');
+    console.log('bb');
+}
+// a();
+
+
+/* task -03 */
+
