213d32437239828cd1c5e6ee419209ee807caf18
imran2024bd <imranitbd34@gmail.com>
2024-03-02 21:11:53 +0600

    js more concept done

---
+++ task.js
@@ -0,0 +1,53 @@
+/* ২) নিচের লিঙ্ক দুইটার কোনোটাতে চলে যান। 
+নিজের মত করে কিছু Practise করে ফেলুন Google/ Chatgpt থেকে 
+Example নিয়ে Event Loop এর, আমি একটা Example এখানে Video তে দেখিয়েছি। 
+আপনিও কাছাকাছি টাইপের কিছু Example নিজে নিজে ট্রাই করলে Event Loop এর পুরো Flow টা বুঝে যাবেন।
+
+Link-1: Http://Latentflip.Com/Loupe/
+
+
+
+Link-2: Https://Www.Jsv9000.App/
+*/
+
+
+
+
+
+/*  Dont understand 
+GOOOOOOOOOOO     Toooooooooooo
+
+http://latentflip.com/loupe
+
+https://www.jsv9000.app
+
+
+
+*/
+
+
+async function f() {
+    let result = 'first!';
+    let promise = new Promise((resolve, reject) => {
+     setTimeout(() => resolve('done!'), 1000);
+    });
+    result = await promise;
+    console.log(result);
+   }
+   f();
+
+
+   /* আজকে কাজ হচ্ছে মোট ৫ টা ।  
+
+১ । মডিউলে থাকা ৩ টা ভিডিও সুন্দর করে দেখে ফেলো। 
+
+২। এবারে কাজ হলো প্র্যাক্টিস করা। নিচের লিংক এ ক্লিক করে পুরো বিষয় টা প্র্যাক্টিস করার জন্যে একটা টেস্ট এসাইনমেন্ট রিপোজিটরি তৈরী করে ফেলো। 
+
+Https://Classroom.Github.Com/A/NEdAa_d3
+
+৩। এবার এই রিপোজিটোরি কে তোমার নতুন প্রোজেক্ট এর লোকাল ফোল্ডারের সাথে কানেক্ট করে ফেলো এবং রিপোজিটোরিতে কমপক্ষে ৩ টা কমিট করো এবং পুশ করো।  
+
+৪। এবারে নেটলিফাই ব্যবহার করে এই প্রোজেক্ট টা ডিপ্লয় করো। 
+
+৫। এবারে Surge ব্যবহার করে এই প্রোজেক্ট টা ডিপ্লয় করো। 
+*/
